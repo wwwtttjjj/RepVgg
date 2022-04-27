e4b4d7592fe613c6aee5d5ee72e7863e0e75dc96
来年 <43205622+wwwtttjjj@users.noreply.github.com>
2022-04-27 20:59:12 +0800

    Add files via upload

--- trainDateParrelle.py
+++ trainDateParrelle.py
@@ -5,17 +5,27 @@ import os
 import model
 import wandb
 import argparse
+import torch.distributed
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data.distributed import DistributedSampler
+
 parse = argparse.ArgumentParser(description='input parameter of trian')
 
-parse.add_argument('--batch-size', help = 'the size of training datesets', default=8,type=int)
+parse.add_argument('--batch-size', help = 'the size of training datesets', default=64,type=int)
 parse.add_argument('--lr', help = 'lr', default=1e-3,type=float)
-parse.add_argument('--epochs', help = 'nums of train', default=50,type=int)
-parse.add_argument('--wandb', help = 'use or not use wandb', default=False, type=bool)
+parse.add_argument('--epochs', help = 'nums of train', default=50)
+parse.add_argument('--wandb', help = 'use or not use wandb', default=True, type=bool)
 # parse.add_argument('--yaml', help = 'yaml file path, edit', type = str)
 args = parse.parse_args()
 
-batch_size = args.batch_size
-device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
+#init more gpus
+
+local_rank = int(os.environ['LOCAL_RANK'])
+world_size = int(os.environ["WORLD_SIZE"])
+torch.distributed.init_process_group(backend='nccl', rank = local_rank, world_size = world_size)
+device = torch.device('cuda', local_rank)
+
+# device = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
 '''
 data
 '''
@@ -28,19 +38,20 @@ transform = torchvision.transforms.Compose([
     torchvision.transforms.Normalize(mean = [0.485, 0.456,0.406],std = [0.229, 0.224, 0.225])
 ])
 data_iter = torchvision.datasets.ImageFolder(root=train_root, transform=transform)
-data_load = torch.utils.data.DataLoader(data_iter, batch_size = args.batch_size, shuffle = True)
+data_load = torch.utils.data.DataLoader(data_iter, batch_size = args.batch_size, shuffle = False, sampler = DistributedSampler(data_iter))
 
 test_iter = torchvision.datasets.ImageFolder(root=test_root, transform=transform)
-test_load = torch.utils.data.DataLoader(test_iter, batch_size = args.batch_size, shuffle = False)
+test_load = torch.utils.data.DataLoader(test_iter, batch_size = args.batch_size, shuffle = False, sampler = DistributedSampler(test_iter))
 
 lossf = torch.nn.CrossEntropyLoss()
-if args.wandb:
+if args.wandb and local_rank == 0:
     wandb.init(entity='wtj', project='Repvgg object classifier')
     wandb.config = {
         'lr':args.lr,
         'epochs':args.epochs, 
         'batch_size':args.batch_size
     }
+
 if os.path.exists('./RepVgg.pt'):
     modelRepVgg = torch.load('./RepVgg.pt', map_location=torch.device(device))
 else:
@@ -48,9 +59,9 @@ else:
 
 if torch.cuda.device_count() > 1:
     print('use',torch.cuda.device_count(),'gpus')
-    modelRepVgg = torch.nn.DataParallel(modelRepVgg)
-
+    modelRepVgg = DDP(modelRepVgg, device_ids = [local_rank], output_device = local_rank)
 modelRepVgg.train()
+
 optimizer = torch.optim.SGD(modelRepVgg.parameters(), lr = args.lr)
 
 for eopch in range(args.epochs):
@@ -78,13 +89,14 @@ for eopch in range(args.epochs):
             correct +=  (y_p == label).sum()
             total += args.batch_size
         test_acc = correct / total
-        print(test_acc, train_mean_loss)
-        if args.wandb:
-            wandb.log({'train_loss':train_mean_loss,'test_acc:':test_acc})    
+        print(local_rank,"test acc is",test_acc.item(), "loss is",train_mean_loss.item())
+        if args.wandb and local_rank == 0:
+            wandb.log({'train_mean_loss':train_mean_loss,'test_acc:':test_acc})    
     modelRepVgg.train()
 #保存模型，保存转化了和没有转化的模型
-torch.save(modelRepVgg, './RepVgg.pt')
-torch.save(model.repVgg_model_convert(modelRepVgg), './RepVgg_Convert.pt')
+    torch.save(modelRepVgg.module, './RepVgg.pt')
+    torch.save(model.repVgg_model_convert(modelRepVgg.module), './RepVgg_Convert.pt')
 
     
+
 
